7b62dc7492120a1b2569e82dca127ebe9e865a9f
YiDongYeol <ph31075421@gmail.com>
2022-06-07 17:42:21 +0900

    220607 네이버 뉴스 검색앱 완료, MVVM 학습

---
+++ DAY05/WpfMvvmApp/ViewModels/RelayCommand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfMvvmApp.ViewModels
+{
+    /// <summary>
+    /// ViewModel과 View를 Glue하기 위한 클래스
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> execute;     //실행처리를 위한 Generic
+        private readonly Predicate<T> canExecute;       //실행여부를 판단하는 Generic
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute?.Invoke((T)parameter) ?? true;
+        }
+        public void Execute(object parameter)
+        {
+            execute((T)parameter);
+        }
+
+        /// <summary>
+        /// execute만 파라미터 받는 생성자
+        /// </summary>
+        /// <param name="execute"></param>
+        public RelayCommand(Action<T> execute) : this(execute, null) { }
+
+        /// <summary>
+        /// execute, canExecute를 파라미터로 받는 생성자
+        /// </summary>
+        /// <param name="execute"></param>
+        /// <param name="canExecute"></param>
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute = null)
+        {
+            this.execute = execute ?? throw new ArgumentException(nameof(execute));
+            this.canExecute = canExecute;
+        }
+    }
+}
